3a60e6dcd9bd440fbb06c1586853745fad8eec77
Lucas Brendel <lucasbrendel@live.com>
2012-06-17 13:44:33 -0500

    General Update
    - Added pages for navigation
    - App bar icons

--- LiquidGold/LiquidGold/MainPage.xaml.cs
+++ LiquidGold/LiquidGold/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,10 +16,16 @@ namespace LiquidGold
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            List<ViewModel.Vehicle> vehicles = new List<ViewModel.Vehicle>();
+            vehicles.Add(new ViewModel.Vehicle() { Name = "Mazda", Image = "Icons/appbar.add.dark.png" });
+            vehicles.Add(new ViewModel.Vehicle() { Name = "Toyota", Image = "Icons/appbar.cancel.dark.png" });
+
+            VehicleList.ItemsSource = vehicles;
         }
     }
 }
---
+++ LiquidGold/LiquidGold/ViewModel/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace LiquidGold.ViewModel
+{
+    public class Vehicle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _image;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                if (value != _image)
+                {
+                    _image = value;
+                }
+            }
+        }
+    }
+}
