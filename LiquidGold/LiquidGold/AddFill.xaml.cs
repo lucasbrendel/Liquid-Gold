e9b1f87ce9863ff36c8774eebb6a1300a51626a9
Lucas Brendel <lucasbrendel@live.com>
2012-06-17 20:43:19 -0500

    Updates to AddFill

--- LiquidGold/LiquidGold/AddFill.xaml.cs
+++ LiquidGold/LiquidGold/AddFill.xaml.cs
@@ -17,14 +17,20 @@ namespace LiquidGold
     public partial class AddFill : PhoneApplicationPage
     {
         private ViewModel.VehicleDataContext vehicleDb;
+        private ViewModel.FillUpDataContext fillUpDb;
 
         private ObservableCollection<ViewModel.Vehicle> _vehicles;
+        private ObservableCollection<ViewModel.FillUp> _fills;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public AddFill()
         {
             InitializeComponent();
 
             vehicleDb = new ViewModel.VehicleDataContext(ViewModel.VehicleDataContext.VehicleConnectionString);
+            fillUpDb = new ViewModel.FillUpDataContext(ViewModel.FillUpDataContext.DBConnectionString);
         }
 
         /// <summary>
@@ -37,7 +43,9 @@ namespace LiquidGold
             NavigationContext.QueryString.TryGetValue("selectedIndex", out index);
 
             var vehItemsInDB = from ViewModel.Vehicle veh in vehicleDb.VehicleItems select veh;
+            var fillItemsInDB = from ViewModel.FillUp fill in fillUpDb.FillUpItems select fill;
             _vehicles = new ObservableCollection<ViewModel.Vehicle>(vehItemsInDB);
+            _fills = new ObservableCollection<ViewModel.FillUp>(fillItemsInDB);
 
             VehiclesList.ItemsSource = _vehicles;
 
@@ -47,18 +55,39 @@ namespace LiquidGold
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void AddFill_Click(object sender, EventArgs e)
         {
-            string _name = "";
-            ViewModel.FillUp fill = new ViewModel.FillUp(){
-                VehicleName = _name, 
-                Odometer = Convert.ToInt32(Odo_txt.Text), 
-                Quantity = Convert.ToDouble(Quantity_txt.Text), 
-                Cost = Convert.ToDouble(Cost_txt.Text), 
-                Date = (DateTime)FillDate.Value 
-            };
+            if (Odo_txt.Text != String.Empty && Quantity_txt.Text != String.Empty && Cost_txt.Text != String.Empty)
+            {
+                ViewModel.Vehicle _vehicle = (ViewModel.Vehicle)VehiclesList.SelectedItem;
+                ViewModel.FillUp fill = new ViewModel.FillUp()
+                {
+                    VehicleName = _vehicle.Name,
+                    Odometer = Convert.ToInt32(Odo_txt.Text),
+                    Quantity = Convert.ToDouble(Quantity_txt.Text),
+                    Cost = Convert.ToDouble(Cost_txt.Text),
+                    Date = (DateTime)FillDate.Value
+                };
+
+                fillUpDb.FillUpItems.InsertOnSubmit(fill);
+                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+            }
+            else
+            {
+                MessageBox.Show("All values must be filled", "ERROR", MessageBoxButton.OK);
+            }
         }
 
         /// <summary>
@@ -68,7 +97,7 @@ namespace LiquidGold
         /// <param name="e"></param>
         private void CancelFill_Click(object sender, EventArgs e)
         {
-
-        }
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }   
     }
 }
